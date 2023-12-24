d41123aafc2bdd6186b5d751366037be8c0ea184
NishanthThummuru <154494072+NishanthThummuru@users.noreply.github.com>
2023-12-24 18:22:37 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -47,41 +47,10 @@ function Login() {
 
 
   return (
-    // <div id="go">
-    //   <div className="container" style={{ width: "400px", marginTop: "100px" }}>
-    //     <div class="card" id="g" >
-    //       <div class="card-body">
+   
 
-    //         <form onSubmit={submitData}>
-    //           <h2 style={{ textAlign: "center", }}>Login</h2>
-
-    //           <div class="mb-3">
-    //             <label class="form-label">Email address</label>
-    //             <input type="email" required class="form-control" value={email} onChange={e => setEmail(e.target.value)} ></input>
-    //           </div>
-    //           <div class="mb-3">
-    //             <label class="form-label">Password</label>
-    //             <input type="password" required class="form-control" value={pswd} onChange={e => setPswd(e.target.value)}  ></input>
-    //           </div>
-
-
-    //           <button type="submit" class="btn btn-primary">Login</button>
-    //         </form>
-    //         <br></br>
-    //         <div>
-    //           <a ><b>If you dont't have an account </b>
-    //             <Link to="/" style={{color:"white",textDecorationLine:"none", marginLeft:"10px",fontSize:"larger"}}>Register</Link>
-    //           </a>
-    //         </div>
-
-
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
-    <div id="login" >
-      <h2 id="h" > <span style={{ color: "purple" }}> Mobile</span><span style={{ color: "pink" }}> Store</span></h2>
+    <div id="signup" >
+      <h2 id="h" ><b><span style={{ color: "purple" }}> Mobile</span><span style={{ color: "pink" }}> Store</span> </b> </h2>
       <div id="form" >
 
 
@@ -92,23 +61,23 @@ function Login() {
             <br></br>
             <label><b style={{ fontSize: "larger" }}>E-mail:</b></label>
             <br></br>
-            <input style={{ textAlign: "center", width: "330px", padding: "5px", borderRadius: "5px" }} type="email" required placeholder="enter your e-mail" value={email} onChange={e => setEmail(e.target.value)}></input>
+            <input id="in" type="email" required placeholder="enter your e-mail" value={email} onChange={e => setEmail(e.target.value)}></input>
           </div>
           <div>
             <br></br>
             <label><b style={{ fontSize: "larger" }}>Password:</b></label>
             <br></br>
-            <input style={{ textAlign: "center", width: "330px", padding: "5px", borderRadius: "5px" }} type="password" required placeholder="password" value={pswd} onChange={e => setPswd(e.target.value)} ></input>
+            <input id="in" type="password" required placeholder="password" value={pswd} onChange={e => setPswd(e.target.value)} ></input>
           </div>
           <br></br>
 
 
-          <button type="submit" class="btn btn-primary" style={{ marginLeft: "60px", width: "200px", padding: "8px", color: "white", backgroundColor: " rgb(8, 4, 70)" }}>Login</button>
+          <button id="re" type="submit" class="btn btn-primary" >Login</button>
         </form>
 
         <br></br>
         <a><b>Don't have an account?</b>
-          <Link to="/" style={{ textDecorationLine: "none", marginLeft: "10px", fontSize: "larger", color: "blue" }}>Register</Link>
+          <Link to="/" style={{ textDecorationLine: "none", marginLeft: "10px", fontSize: "larger", color: "white" }}>Register</Link>
         </a>
 
       </div>
