ff86bcb7000f357e3ffd82873c8e02aaf564b574
NishanthThummuru <154494072+NishanthThummuru@users.noreply.github.com>
2023-12-24 18:22:03 +0530

    Update Signup.js

--- src/Signup.js
+++ src/Signup.js
@@ -52,7 +52,7 @@ function Signup() {
     return (
 
         <div id="signup" >
-            <h2 id="h" > <span style={{ color: "purple" }}> Mobile</span><span style={{ color: "pink" }}> Store</span></h2>
+            <h2 id="h" ><b> <span style={{ color: "purple" }}> Mobile</span><span style={{ color: "pink" }}> Store</span> </b></h2>
             <div id="blur" >
 
 
@@ -61,34 +61,34 @@ function Signup() {
                     <div>
                         <label  ><b style={{ fontSize: "larger" }}>Name:</b></label>
                         <br></br>
-                        <input style={{ textAlign: "center", width: "330px", padding: "5px", borderRadius: "5px" }} type="text" required placeholder="enter your full name"  value={name} onChange={e => setName(e.target.value)}  ></input>
+                        <input  id="in"  type="text" required placeholder="enter your full name"   value={name} onChange={e => setName(e.target.value)}  ></input>
                     </div>
                     <div>
                         <br></br>
                         <label><b style={{ fontSize: "larger" }}>E-mail:</b></label>
                         <br></br>
-                        <input style={{ textAlign: "center", width: "330px", padding: "5px", borderRadius: "5px" }} type="email" required placeholder="enter your e-mail" value={email} onChange={e => setEmail(e.target.value)}></input>
+                        <input id="in"  type="email" required placeholder="enter your e-mail" value={email} onChange={e => setEmail(e.target.value)}></input>
                     </div>
                     <div>
                         <br></br>
-                        <label><b style={{ fontSize: "larger" }}>Password:</b></label>
+                        <label ><b style={{ fontSize: "larger" }}>Password:</b></label>
                         <br></br>
-                        <input style={{ textAlign: "center", width: "330px", padding: "5px", borderRadius: "5px" }} type="password" required placeholder="password" value={pswd} onChange={e => setPswd(e.target.value)} ></input>
+                        <input id="in"  type="password" required placeholder="password" value={pswd} onChange={e => setPswd(e.target.value)} ></input>
                     </div>
                     <br></br>
                     <div>
                         <label><b style={{ fontSize: "larger" }}>Confirm-password:</b></label>
                         <br></br>
-                        <input style={{ textAlign: "center", width: "330px", padding: "5px", borderRadius: "5px" }} type="password" required placeholder="confirm-password" value={cpswd} onChange={e => setCpswd(e.target.value)}  ></input>
+                        <input id="in"  type="password" required placeholder="confirm-password" value={cpswd} onChange={e => setCpswd(e.target.value)}  ></input>
                     </div>
                     <br></br>
 
-                    <button type="submit" class="btn btn-primary" style={{ marginLeft: "60px", width: "200px", padding: "8px", color: "white", backgroundColor: " rgb(8, 4, 70)" }}>Register</button>
+                    <button id="re" type="submit" class="btn btn-primary" style={{ }}>Register</button>
                 </form>
 
                 <br></br>
                 <a><b>If you have an account</b>
-                    <Link to="/Login" style={{ textDecorationLine: "none", marginLeft: "10px", fontSize: "larger", color: "blue" }}>Login here</Link>
+                    <Link to="/Login" style={{ textDecorationLine: "none", marginLeft: "10px", fontSize: "larger", color: "white" }}>Login here</Link>
                 </a>
 
             </div>
